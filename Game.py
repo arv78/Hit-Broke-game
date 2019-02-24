f1096a04c70303df7a7faafc8656a04b38b8a73f
Arya <arv78@ymail.com>
2019-02-24 21:04:47 +0330

    On branch master  Initial commit  Changes to be committed: 	new file:   Game.py

---
+++ Game.py
@@ -0,0 +1,16 @@
+# input (file or user_input or random)
+# choose the starter of the game
+
+#assign the inputs to the global Arrays
+#define func (distance):
+    ''' for Arr1 (i)
+           for Arr2 (j)
+               if Arr1[i] + Arr2[j] < distance then
+               ** we need a counter to determine how many lvl's we should continue **
+                  func (distance -(Arr1[i] + Arr2[j]))  *(still in progress)*
+               elif Arr1[i] > distance then
+                  return true  *(we won)*
+               else
+                  break
+'''
+    
